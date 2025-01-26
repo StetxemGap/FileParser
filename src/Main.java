2adc2432fa2fb7b499f8c41d09d81fa96fc90e53
StetXem <sisisisi823@gmail.com>
2025-01-26 17:36:10 +0700

    Выходные файлы создаются по необходимости с учетом опций -o -p

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,32 +11,41 @@ public class Main {
     private static boolean optionS = false; //краткая статистика(сколько было добавлено в каждый из выходных файлов)
     private static boolean optionF = false; //полная статистика(числа: min, max, sum, mu; строки: min, max)
 
+    private static String resultPath;
+    private static String resultPrefix;
+
     public static void main(String[] args) {
         checkOptions(args);
-        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<String> integers = new ArrayList<>();
+        ArrayList<String> floats = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/in1.txt"))) {
             String line;
+
             while ((line = bufferedReader.readLine()) != null) {
-                lines.add(line);
+                if (isInteger(line)) {
+                    integers.add(line);
+                } else if (isFloat(line)) {
+                    floats.add(line);
+                } else {
+                    strings.add(line);
+                }
             }
-            cheсkType(lines);
+
+            if (!integers.isEmpty()) {
+                fileCreator("integers.txt");
+            }
+
+            if (!floats.isEmpty()) {
+                fileCreator("floats.txt");
+            }
+
+            if (!strings.isEmpty()) {
+                fileCreator("strings.txt");
+            }
+
         } catch (IOException e) {
             System.err.println("Ошибка чтения файла: " + e.getMessage());
-        }
-    }
-
-    public static void cheсkType(ArrayList<String> lines) {
-        for (String line : lines) {
-            if (isInteger(line)) {
-                System.out.println(line + " - целое число");
-                writeToFile(line, 1);
-            } else if (isFloat(line)) {
-                System.out.println(line + " - вещественное число");
-                writeToFile(line, 2);
-            } else {
-                System.out.println(line + " - строка");
-                writeToFile(line, 3);
-            }
         }
     }
 
@@ -57,20 +67,39 @@ public class Main {
         }
     }
 
-    public static void writeToFile(String content, Integer Type) {
+    public static void fileCreator(String filename) throws IOException {
+        if (optionP) {filename = resultPrefix + filename;}
+        if (optionO) {filename = resultPath + filename;}
+
+        try {
+            File file = new File(filename);
+            if (file.createNewFile()) {
+                System.out.println("Файл" + filename + "создан");
+            } else {
+                System.out.println("Файл" + filename + "уже существует");
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при создании файла");
+            e.printStackTrace();
+        }
 
     }
 
     public static void checkOptions(String[] args){
+        System.out.println("\n*************************** Анализ операций ***************************\n");
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
                 case "-o":
                     optionO = true;
                     System.out.println("Опция о применена");
+                    resultPath = args[++i];
+                    System.out.println("Путь для результатов: " + resultPath);
                     break;
                 case "-p":
                     optionP = true;
                     System.out.println("Опция p применена");
+                    resultPrefix = args[++i];
+                    System.out.println("Префикс для файлов с результатом: " + resultPrefix);
                     break;
                 case "-a":
                     optionA = true;
